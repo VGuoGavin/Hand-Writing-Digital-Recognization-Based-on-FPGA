83b496051b86f3b85c0ea9c45c7d81a4a275d82d
VGuoGavin <53364849+VGuoGavin@users.noreply.github.com>
2021-11-17 15:30:59 +0800

    Add files via upload

--- bus/sys_ahb/L2AhbMtx/L2AhbMtx.v
+++ bus/sys_ahb/L2AhbMtx/L2AhbMtx.v
@@ -1,1 +1,460 @@
+//-----------------------------------------------------------------------------
+// The confidential and proprietary information contained in this file may
+// only be used by a person authorised under and to the extent permitted
+// by a subsisting licensing agreement from ARM Limited.
+//
+//            (C) COPYRIGHT 2001-2013-2020 ARM Limited.
+//                ALL RIGHTS RESERVED
+//
+// This entire notice must be reproduced on all copies of this file
+// and copies of this file may only be made by a person if such person is
+// permitted to do so under the terms of a subsisting license agreement
+// from ARM Limited.
+//
+//      SVN Information
+//
+//      Checked In          : $Date: 2012-10-15 18:01:36 +0100 (Mon, 15 Oct 2012) $
+//
+//      Revision            : $Revision: 225465 $
+//
+//      Release Information : Cortex-M System Design Kit-r1p0-01rel0
+//
+//-----------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+//  Abstract            : BusMatrix is the top-level which connects together
+//                        the required Input Stages, MatrixDecodes, Output
+//                        Stages and Output Arbitration blocks.
+//
+//                        Supports the following configured options:
+//
+//                         - Architecture type 'ahb2',
+//                         - 1 slave ports (connecting to masters),
+//                         - 2 master ports (connecting to slaves),
+//                         - Routing address width of 32 bits,
+//                         - Routing data width of 32 bits,
+//                         - Arbiter type 'round',
+//                         - Connectivity mapping:
+//                             S<0..0> -> M<0..1>,
+//                         - Connectivity type 'full'.
+//
+//------------------------------------------------------------------------------
 
+`timescale 1ns/1ps
+
+module L2AhbMtx (
+
+    // Common AHB signals
+    HCLK,
+    HRESETn,
+
+    // System address remapping control
+    REMAP,
+
+    // Input port SI0 (inputs from master 0)
+    HSELS0,
+    HADDRS0,
+    HTRANSS0,
+    HWRITES0,
+    HSIZES0,
+    HBURSTS0,
+    HPROTS0,
+    HMASTERS0,
+    HWDATAS0,
+    HMASTLOCKS0,
+    HREADYS0,
+
+    // Output port MI0 (inputs from slave 0)
+    HRDATAM0,
+    HREADYOUTM0,
+    HRESPM0,
+
+    // Output port MI1 (inputs from slave 1)
+    HRDATAM1,
+    HREADYOUTM1,
+    HRESPM1,
+
+    // Scan test dummy signals; not connected until scan insertion
+    SCANENABLE,   // Scan Test Mode Enable
+    SCANINHCLK,   // Scan Chain Input
+
+
+    // Output port MI0 (outputs to slave 0)
+    HSELM0,
+    HADDRM0,
+    HTRANSM0,
+    HWRITEM0,
+    HSIZEM0,
+    HBURSTM0,
+    HPROTM0,
+    HMASTERM0,
+    HWDATAM0,
+    HMASTLOCKM0,
+    HREADYMUXM0,
+
+    // Output port MI1 (outputs to slave 1)
+    HSELM1,
+    HADDRM1,
+    HTRANSM1,
+    HWRITEM1,
+    HSIZEM1,
+    HBURSTM1,
+    HPROTM1,
+    HMASTERM1,
+    HWDATAM1,
+    HMASTLOCKM1,
+    HREADYMUXM1,
+
+    // Input port SI0 (outputs to master 0)
+    HRDATAS0,
+    HREADYOUTS0,
+    HRESPS0,
+
+    // Scan test dummy signals; not connected until scan insertion
+    SCANOUTHCLK   // Scan Chain Output
+
+    );
+
+
+// -----------------------------------------------------------------------------
+// Input and Output declarations
+// -----------------------------------------------------------------------------
+
+    // Common AHB signals
+    input         HCLK;            // AHB System Clock
+    input         HRESETn;         // AHB System Reset
+
+    // System address remapping control
+    input   [3:0] REMAP;           // REMAP input
+
+    // Input port SI0 (inputs from master 0)
+    input         HSELS0;          // Slave Select
+    input  [31:0] HADDRS0;         // Address bus
+    input   [1:0] HTRANSS0;        // Transfer type
+    input         HWRITES0;        // Transfer direction
+    input   [2:0] HSIZES0;         // Transfer size
+    input   [2:0] HBURSTS0;        // Burst type
+    input   [3:0] HPROTS0;         // Protection control
+    input   [3:0] HMASTERS0;       // Master select
+    input  [31:0] HWDATAS0;        // Write data
+    input         HMASTLOCKS0;     // Locked Sequence
+    input         HREADYS0;        // Transfer done
+
+    // Output port MI0 (inputs from slave 0)
+    input  [31:0] HRDATAM0;        // Read data bus
+    input         HREADYOUTM0;     // HREADY feedback
+    input   [1:0] HRESPM0;         // Transfer response
+
+    // Output port MI1 (inputs from slave 1)
+    input  [31:0] HRDATAM1;        // Read data bus
+    input         HREADYOUTM1;     // HREADY feedback
+    input   [1:0] HRESPM1;         // Transfer response
+
+    // Scan test dummy signals; not connected until scan insertion
+    input         SCANENABLE;      // Scan enable signal
+    input         SCANINHCLK;      // HCLK scan input
+
+
+    // Output port MI0 (outputs to slave 0)
+    output        HSELM0;          // Slave Select
+    output [31:0] HADDRM0;         // Address bus
+    output  [1:0] HTRANSM0;        // Transfer type
+    output        HWRITEM0;        // Transfer direction
+    output  [2:0] HSIZEM0;         // Transfer size
+    output  [2:0] HBURSTM0;        // Burst type
+    output  [3:0] HPROTM0;         // Protection control
+    output  [3:0] HMASTERM0;       // Master select
+    output [31:0] HWDATAM0;        // Write data
+    output        HMASTLOCKM0;     // Locked Sequence
+    output        HREADYMUXM0;     // Transfer done
+
+    // Output port MI1 (outputs to slave 1)
+    output        HSELM1;          // Slave Select
+    output [31:0] HADDRM1;         // Address bus
+    output  [1:0] HTRANSM1;        // Transfer type
+    output        HWRITEM1;        // Transfer direction
+    output  [2:0] HSIZEM1;         // Transfer size
+    output  [2:0] HBURSTM1;        // Burst type
+    output  [3:0] HPROTM1;         // Protection control
+    output  [3:0] HMASTERM1;       // Master select
+    output [31:0] HWDATAM1;        // Write data
+    output        HMASTLOCKM1;     // Locked Sequence
+    output        HREADYMUXM1;     // Transfer done
+
+    // Input port SI0 (outputs to master 0)
+    output [31:0] HRDATAS0;        // Read data bus
+    output        HREADYOUTS0;     // HREADY feedback
+    output  [1:0] HRESPS0;         // Transfer response
+
+    // Scan test dummy signals; not connected until scan insertion
+    output        SCANOUTHCLK;     // Scan Chain Output
+
+
+// -----------------------------------------------------------------------------
+// Wire declarations
+// -----------------------------------------------------------------------------
+
+    // Common AHB signals
+    wire         HCLK;            // AHB System Clock
+    wire         HRESETn;         // AHB System Reset
+
+    // System address remapping control
+    wire   [3:0] REMAP;           // REMAP signal
+
+    // Input Port SI0
+    wire         HSELS0;          // Slave Select
+    wire  [31:0] HADDRS0;         // Address bus
+    wire   [1:0] HTRANSS0;        // Transfer type
+    wire         HWRITES0;        // Transfer direction
+    wire   [2:0] HSIZES0;         // Transfer size
+    wire   [2:0] HBURSTS0;        // Burst type
+    wire   [3:0] HPROTS0;         // Protection control
+    wire   [3:0] HMASTERS0;       // Master select
+    wire  [31:0] HWDATAS0;        // Write data
+    wire         HMASTLOCKS0;     // Locked Sequence
+    wire         HREADYS0;        // Transfer done
+
+    wire  [31:0] HRDATAS0;        // Read data bus
+    wire         HREADYOUTS0;     // HREADY feedback
+    wire   [1:0] HRESPS0;         // Transfer response
+
+    // Output Port MI0
+    wire         HSELM0;          // Slave Select
+    wire  [31:0] HADDRM0;         // Address bus
+    wire   [1:0] HTRANSM0;        // Transfer type
+    wire         HWRITEM0;        // Transfer direction
+    wire   [2:0] HSIZEM0;         // Transfer size
+    wire   [2:0] HBURSTM0;        // Burst type
+    wire   [3:0] HPROTM0;         // Protection control
+    wire   [3:0] HMASTERM0;       // Master select
+    wire  [31:0] HWDATAM0;        // Write data
+    wire         HMASTLOCKM0;     // Locked Sequence
+    wire         HREADYMUXM0;     // Transfer done
+
+    wire  [31:0] HRDATAM0;        // Read data bus
+    wire         HREADYOUTM0;     // HREADY feedback
+    wire   [1:0] HRESPM0;         // Transfer response
+
+    // Output Port MI1
+    wire         HSELM1;          // Slave Select
+    wire  [31:0] HADDRM1;         // Address bus
+    wire   [1:0] HTRANSM1;        // Transfer type
+    wire         HWRITEM1;        // Transfer direction
+    wire   [2:0] HSIZEM1;         // Transfer size
+    wire   [2:0] HBURSTM1;        // Burst type
+    wire   [3:0] HPROTM1;         // Protection control
+    wire   [3:0] HMASTERM1;       // Master select
+    wire  [31:0] HWDATAM1;        // Write data
+    wire         HMASTLOCKM1;     // Locked Sequence
+    wire         HREADYMUXM1;     // Transfer done
+
+    wire  [31:0] HRDATAM1;        // Read data bus
+    wire         HREADYOUTM1;     // HREADY feedback
+    wire   [1:0] HRESPM1;         // Transfer response
+
+
+// -----------------------------------------------------------------------------
+// Signal declarations
+// -----------------------------------------------------------------------------
+
+    // Bus-switch input SI0
+    wire         i_sel0;            // HSEL signal
+    wire  [31:0] i_addr0;           // HADDR signal
+    wire   [1:0] i_trans0;          // HTRANS signal
+    wire         i_write0;          // HWRITE signal
+    wire   [2:0] i_size0;           // HSIZE signal
+    wire   [2:0] i_burst0;          // HBURST signal
+    wire   [3:0] i_prot0;           // HPROTS signal
+    wire   [3:0] i_master0;         // HMASTER signal
+    wire         i_mastlock0;       // HMASTLOCK signal
+    wire         i_active0;         // Active signal
+    wire         i_held_tran0;       // HeldTran signal
+    wire         i_readyout0;       // Readyout signal
+    wire   [1:0] i_resp0;           // Response signal
+
+    // Bus-switch SI0 to MI0 signals
+    wire         i_sel0to0;         // Routing selection signal
+    wire         i_active0to0;      // Active signal
+
+    // Bus-switch SI0 to MI1 signals
+    wire         i_sel0to1;         // Routing selection signal
+    wire         i_active0to1;      // Active signal
+
+    wire         i_hready_mux_m0;    // Internal HREADYMUXM for MI0
+    wire         i_hready_mux_m1;    // Internal HREADYMUXM for MI1
+
+
+// -----------------------------------------------------------------------------
+// Beginning of main code
+// -----------------------------------------------------------------------------
+
+  // Input stage for SI0
+  L2AhbInStg u_L2AhbInStg_0 (
+
+    // Common AHB signals
+    .HCLK       (HCLK),
+    .HRESETn    (HRESETn),
+
+    // Input Port Address/Control Signals
+    .HSELS      (HSELS0),
+    .HADDRS     (HADDRS0),
+    .HTRANSS    (HTRANSS0),
+    .HWRITES    (HWRITES0),
+    .HSIZES     (HSIZES0),
+    .HBURSTS    (HBURSTS0),
+    .HPROTS     (HPROTS0),
+    .HMASTERS   (HMASTERS0),
+    .HMASTLOCKS (HMASTLOCKS0),
+    .HREADYS    (HREADYS0),
+
+    // Internal Response
+    .active_ip     (i_active0),
+    .readyout_ip   (i_readyout0),
+    .resp_ip       (i_resp0),
+
+    // Input Port Response
+    .HREADYOUTS (HREADYOUTS0),
+    .HRESPS     (HRESPS0),
+
+    // Internal Address/Control Signals
+    .sel_ip        (i_sel0),
+    .addr_ip       (i_addr0),
+    .trans_ip      (i_trans0),
+    .write_ip      (i_write0),
+    .size_ip       (i_size0),
+    .burst_ip      (i_burst0),
+    .prot_ip       (i_prot0),
+    .master_ip     (i_master0),
+    .mastlock_ip   (i_mastlock0),
+    .held_tran_ip   (i_held_tran0)
+
+    );
+
+
+  // Matrix decoder for SI0
+  L2AhbDecS0 u_l2ahbdecs0 (
+
+    // Common AHB signals
+    .HCLK       (HCLK),
+    .HRESETn    (HRESETn),
+
+    // Signals from Input stage SI0
+    .HREADYS    (HREADYS0),
+    .sel_dec        (i_sel0),
+    .decode_addr_dec (i_addr0[31:10]),   // HADDR[9:0] is not decoded
+    .trans_dec      (i_trans0),
+
+    // Control/Response for Output Stage MI0
+    .active_dec0    (i_active0to0),
+    .readyout_dec0  (i_hready_mux_m0),
+    .resp_dec0      (HRESPM0),
+    .rdata_dec0     (HRDATAM0),
+
+    // Control/Response for Output Stage MI1
+    .active_dec1    (i_active0to1),
+    .readyout_dec1  (i_hready_mux_m1),
+    .resp_dec1      (HRESPM1),
+    .rdata_dec1     (HRDATAM1),
+
+    .sel_dec0       (i_sel0to0),
+    .sel_dec1       (i_sel0to1),
+
+    .active_dec     (i_active0),
+    .HREADYOUTS (i_readyout0),
+    .HRESPS     (i_resp0),
+    .HRDATAS    (HRDATAS0)
+
+    );
+
+
+  // Output stage for MI0
+  L2AhbOutStg u_l2ahboutstg_0 (
+
+    // Common AHB signals
+    .HCLK       (HCLK),
+    .HRESETn    (HRESETn),
+
+    // Port 0 Signals
+    .sel_op0       (i_sel0to0),
+    .addr_op0      (i_addr0),
+    .trans_op0     (i_trans0),
+    .write_op0     (i_write0),
+    .size_op0      (i_size0),
+    .burst_op0     (i_burst0),
+    .prot_op0      (i_prot0),
+    .master_op0    (i_master0),
+    .mastlock_op0  (i_mastlock0),
+    .wdata_op0     (HWDATAS0),
+    .held_tran_op0  (i_held_tran0),
+
+    // Slave read data and response
+    .HREADYOUTM (HREADYOUTM0),
+
+    .active_op0    (i_active0to0),
+
+    // Slave Address/Control Signals
+    .HSELM      (HSELM0),
+    .HADDRM     (HADDRM0),
+    .HTRANSM    (HTRANSM0),
+    .HWRITEM    (HWRITEM0),
+    .HSIZEM     (HSIZEM0),
+    .HBURSTM    (HBURSTM0),
+    .HPROTM     (HPROTM0),
+    .HMASTERM   (HMASTERM0),
+    .HMASTLOCKM (HMASTLOCKM0),
+    .HREADYMUXM (i_hready_mux_m0),
+    .HWDATAM    (HWDATAM0)
+
+    );
+
+  // Drive output with internal version
+  assign HREADYMUXM0 = i_hready_mux_m0;
+
+
+  // Output stage for MI1
+  L2AhbOutStg u_l2ahboutstg_1 (
+
+    // Common AHB signals
+    .HCLK       (HCLK),
+    .HRESETn    (HRESETn),
+
+    // Port 0 Signals
+    .sel_op0       (i_sel0to1),
+    .addr_op0      (i_addr0),
+    .trans_op0     (i_trans0),
+    .write_op0     (i_write0),
+    .size_op0      (i_size0),
+    .burst_op0     (i_burst0),
+    .prot_op0      (i_prot0),
+    .master_op0    (i_master0),
+    .mastlock_op0  (i_mastlock0),
+    .wdata_op0     (HWDATAS0),
+    .held_tran_op0  (i_held_tran0),
+
+    // Slave read data and response
+    .HREADYOUTM (HREADYOUTM1),
+
+    .active_op0    (i_active0to1),
+
+    // Slave Address/Control Signals
+    .HSELM      (HSELM1),
+    .HADDRM     (HADDRM1),
+    .HTRANSM    (HTRANSM1),
+    .HWRITEM    (HWRITEM1),
+    .HSIZEM     (HSIZEM1),
+    .HBURSTM    (HBURSTM1),
+    .HPROTM     (HPROTM1),
+    .HMASTERM   (HMASTERM1),
+    .HMASTLOCKM (HMASTLOCKM1),
+    .HREADYMUXM (i_hready_mux_m1),
+    .HWDATAM    (HWDATAM1)
+
+    );
+
+  // Drive output with internal version
+  assign HREADYMUXM1 = i_hready_mux_m1;
+
+
+endmodule
+
+// --================================= End ===================================--
---
+++ bus/sys_ahb/L2AhbMtx/L2AhbMtx_lite.v
@@ -0,0 +1,317 @@
+//-----------------------------------------------------------------------------
+// The confidential and proprietary information contained in this file may
+// only be used by a person authorised under and to the extent permitted
+// by a subsisting licensing agreement from ARM Limited.
+//
+//            (C) COPYRIGHT 2001-2013-2020 ARM Limited.
+//                ALL RIGHTS RESERVED
+//
+// This entire notice must be reproduced on all copies of this file
+// and copies of this file may only be made by a person if such person is
+// permitted to do so under the terms of a subsisting license agreement
+// from ARM Limited.
+//
+//      SVN Information
+//
+//      Checked In          : $Date: 2012-10-15 18:01:36 +0100 (Mon, 15 Oct 2012) $
+//
+//      Revision            : $Revision: 225465 $
+//
+//      Release Information : Cortex-M System Design Kit-r1p0-01rel0
+//
+//-----------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+//  Abstract            : BusMatrixLite is a wrapper module that wraps around
+//                        the BusMatrix module to give AHB Lite compliant
+//                        slave and master interfaces.
+//
+//-----------------------------------------------------------------------------
+
+`timescale 1ns/1ps
+
+module L2AhbMtx_lite (
+
+    // Common AHB signals
+    HCLK,
+    HRESETn,
+
+    // System Address Remap control
+    REMAP,
+
+    // Input port SI0 (inputs from master 0)
+    HADDRS0,
+    HTRANSS0,
+    HWRITES0,
+    HSIZES0,
+    HBURSTS0,
+    HPROTS0,
+    HWDATAS0,
+    HMASTLOCKS0,
+
+    // Output port MI0 (inputs from slave 0)
+    HRDATAM0,
+    HREADYOUTM0,
+    HRESPM0,
+
+    // Output port MI1 (inputs from slave 1)
+    HRDATAM1,
+    HREADYOUTM1,
+    HRESPM1,
+
+    // Scan test dummy signals; not connected until scan insertion
+    SCANENABLE,   // Scan Test Mode Enable
+    SCANINHCLK,   // Scan Chain Input
+
+
+    // Output port MI0 (outputs to slave 0)
+    HSELM0,
+    HADDRM0,
+    HTRANSM0,
+    HWRITEM0,
+    HSIZEM0,
+    HBURSTM0,
+    HPROTM0,
+    HWDATAM0,
+    HMASTLOCKM0,
+    HREADYMUXM0,
+
+    // Output port MI1 (outputs to slave 1)
+    HSELM1,
+    HADDRM1,
+    HTRANSM1,
+    HWRITEM1,
+    HSIZEM1,
+    HBURSTM1,
+    HPROTM1,
+    HWDATAM1,
+    HMASTLOCKM1,
+    HREADYMUXM1,
+
+    // Input port SI0 (outputs to master 0)
+    HRDATAS0,
+    HREADYS0,
+    HRESPS0,
+
+    // Scan test dummy signals; not connected until scan insertion
+    SCANOUTHCLK   // Scan Chain Output
+
+    );
+
+// -----------------------------------------------------------------------------
+// Input and Output declarations
+// -----------------------------------------------------------------------------
+
+    // Common AHB signals
+    input         HCLK;            // AHB System Clock
+    input         HRESETn;         // AHB System Reset
+
+    // System Address Remap control
+    input   [3:0] REMAP;           // System Address REMAP control
+
+    // Input port SI0 (inputs from master 0)
+    input  [31:0] HADDRS0;         // Address bus
+    input   [1:0] HTRANSS0;        // Transfer type
+    input         HWRITES0;        // Transfer direction
+    input   [2:0] HSIZES0;         // Transfer size
+    input   [2:0] HBURSTS0;        // Burst type
+    input   [3:0] HPROTS0;         // Protection control
+    input  [31:0] HWDATAS0;        // Write data
+    input         HMASTLOCKS0;     // Locked Sequence
+
+    // Output port MI0 (inputs from slave 0)
+    input  [31:0] HRDATAM0;        // Read data bus
+    input         HREADYOUTM0;     // HREADY feedback
+    input         HRESPM0;         // Transfer response
+
+    // Output port MI1 (inputs from slave 1)
+    input  [31:0] HRDATAM1;        // Read data bus
+    input         HREADYOUTM1;     // HREADY feedback
+    input         HRESPM1;         // Transfer response
+
+    // Scan test dummy signals; not connected until scan insertion
+    input         SCANENABLE;      // Scan enable signal
+    input         SCANINHCLK;      // HCLK scan input
+
+
+    // Output port MI0 (outputs to slave 0)
+    output        HSELM0;          // Slave Select
+    output [31:0] HADDRM0;         // Address bus
+    output  [1:0] HTRANSM0;        // Transfer type
+    output        HWRITEM0;        // Transfer direction
+    output  [2:0] HSIZEM0;         // Transfer size
+    output  [2:0] HBURSTM0;        // Burst type
+    output  [3:0] HPROTM0;         // Protection control
+    output [31:0] HWDATAM0;        // Write data
+    output        HMASTLOCKM0;     // Locked Sequence
+    output        HREADYMUXM0;     // Transfer done
+
+    // Output port MI1 (outputs to slave 1)
+    output        HSELM1;          // Slave Select
+    output [31:0] HADDRM1;         // Address bus
+    output  [1:0] HTRANSM1;        // Transfer type
+    output        HWRITEM1;        // Transfer direction
+    output  [2:0] HSIZEM1;         // Transfer size
+    output  [2:0] HBURSTM1;        // Burst type
+    output  [3:0] HPROTM1;         // Protection control
+    output [31:0] HWDATAM1;        // Write data
+    output        HMASTLOCKM1;     // Locked Sequence
+    output        HREADYMUXM1;     // Transfer done
+
+    // Input port SI0 (outputs to master 0)
+    output [31:0] HRDATAS0;        // Read data bus
+    output        HREADYS0;     // HREADY feedback
+    output        HRESPS0;         // Transfer response
+
+    // Scan test dummy signals; not connected until scan insertion
+    output        SCANOUTHCLK;     // Scan Chain Output
+
+// -----------------------------------------------------------------------------
+// Wire declarations
+// -----------------------------------------------------------------------------
+
+    // Common AHB signals
+    wire         HCLK;            // AHB System Clock
+    wire         HRESETn;         // AHB System Reset
+
+    // System Address Remap control
+    wire   [3:0] REMAP;           // System REMAP signal
+
+    // Input Port SI0
+    wire  [31:0] HADDRS0;         // Address bus
+    wire   [1:0] HTRANSS0;        // Transfer type
+    wire         HWRITES0;        // Transfer direction
+    wire   [2:0] HSIZES0;         // Transfer size
+    wire   [2:0] HBURSTS0;        // Burst type
+    wire   [3:0] HPROTS0;         // Protection control
+    wire  [31:0] HWDATAS0;        // Write data
+    wire         HMASTLOCKS0;     // Locked Sequence
+
+    wire  [31:0] HRDATAS0;        // Read data bus
+    wire         HREADYS0;     // HREADY feedback
+    wire         HRESPS0;         // Transfer response
+
+    // Output Port MI0
+    wire         HSELM0;          // Slave Select
+    wire  [31:0] HADDRM0;         // Address bus
+    wire   [1:0] HTRANSM0;        // Transfer type
+    wire         HWRITEM0;        // Transfer direction
+    wire   [2:0] HSIZEM0;         // Transfer size
+    wire   [2:0] HBURSTM0;        // Burst type
+    wire   [3:0] HPROTM0;         // Protection control
+    wire  [31:0] HWDATAM0;        // Write data
+    wire         HMASTLOCKM0;     // Locked Sequence
+    wire         HREADYMUXM0;     // Transfer done
+
+    wire  [31:0] HRDATAM0;        // Read data bus
+    wire         HREADYOUTM0;     // HREADY feedback
+    wire         HRESPM0;         // Transfer response
+
+    // Output Port MI1
+    wire         HSELM1;          // Slave Select
+    wire  [31:0] HADDRM1;         // Address bus
+    wire   [1:0] HTRANSM1;        // Transfer type
+    wire         HWRITEM1;        // Transfer direction
+    wire   [2:0] HSIZEM1;         // Transfer size
+    wire   [2:0] HBURSTM1;        // Burst type
+    wire   [3:0] HPROTM1;         // Protection control
+    wire  [31:0] HWDATAM1;        // Write data
+    wire         HMASTLOCKM1;     // Locked Sequence
+    wire         HREADYMUXM1;     // Transfer done
+
+    wire  [31:0] HRDATAM1;        // Read data bus
+    wire         HREADYOUTM1;     // HREADY feedback
+    wire         HRESPM1;         // Transfer response
+
+
+// -----------------------------------------------------------------------------
+// Signal declarations
+// -----------------------------------------------------------------------------
+    wire   [3:0] tie_hi_4;
+    wire         tie_hi;
+    wire         tie_low;
+    wire   [1:0] i_hrespS0;
+
+    wire   [3:0]        i_hmasterM0;
+    wire   [1:0] i_hrespM0;
+    wire   [3:0]        i_hmasterM1;
+    wire   [1:0] i_hrespM1;
+
+// -----------------------------------------------------------------------------
+// Beginning of main code
+// -----------------------------------------------------------------------------
+
+    assign tie_hi   = 1'b1;
+    assign tie_hi_4 = 4'b1111;
+    assign tie_low  = 1'b0;
+
+
+    assign HRESPS0  = i_hrespS0[0];
+
+    assign i_hrespM0 = {tie_low, HRESPM0};
+    assign i_hrespM1 = {tie_low, HRESPM1};
+
+// BusMatrix instance
+  L2AhbMtx uL2AhbMtx (
+    .HCLK       (HCLK),
+    .HRESETn    (HRESETn),
+    .REMAP      (REMAP),
+
+    // Input port SI0 signals
+    .HSELS0       (tie_hi),
+    .HADDRS0      (HADDRS0),
+    .HTRANSS0     (HTRANSS0),
+    .HWRITES0     (HWRITES0),
+    .HSIZES0      (HSIZES0),
+    .HBURSTS0     (HBURSTS0),
+    .HPROTS0      (HPROTS0),
+    .HWDATAS0     (HWDATAS0),
+    .HMASTLOCKS0  (HMASTLOCKS0),
+    .HMASTERS0    (tie_hi_4),
+    .HREADYS0     (HREADYS0),
+    .HRDATAS0     (HRDATAS0),
+    .HREADYOUTS0  (HREADYS0),
+    .HRESPS0      (i_hrespS0),
+
+
+    // Output port MI0 signals
+    .HSELM0       (HSELM0),
+    .HADDRM0      (HADDRM0),
+    .HTRANSM0     (HTRANSM0),
+    .HWRITEM0     (HWRITEM0),
+    .HSIZEM0      (HSIZEM0),
+    .HBURSTM0     (HBURSTM0),
+    .HPROTM0      (HPROTM0),
+    .HWDATAM0     (HWDATAM0),
+    .HMASTERM0    (i_hmasterM0),
+    .HMASTLOCKM0  (HMASTLOCKM0),
+    .HREADYMUXM0  (HREADYMUXM0),
+    .HRDATAM0     (HRDATAM0),
+    .HREADYOUTM0  (HREADYOUTM0),
+    .HRESPM0      (i_hrespM0),
+
+    // Output port MI1 signals
+    .HSELM1       (HSELM1),
+    .HADDRM1      (HADDRM1),
+    .HTRANSM1     (HTRANSM1),
+    .HWRITEM1     (HWRITEM1),
+    .HSIZEM1      (HSIZEM1),
+    .HBURSTM1     (HBURSTM1),
+    .HPROTM1      (HPROTM1),
+    .HWDATAM1     (HWDATAM1),
+    .HMASTERM1    (i_hmasterM1),
+    .HMASTLOCKM1  (HMASTLOCKM1),
+    .HREADYMUXM1  (HREADYMUXM1),
+    .HRDATAM1     (HRDATAM1),
+    .HREADYOUTM1  (HREADYOUTM1),
+    .HRESPM1      (i_hrespM1),
+
+
+    // Scan test dummy signals; not connected until scan insertion
+    .SCANENABLE            (SCANENABLE),
+    .SCANINHCLK            (SCANINHCLK),
+    .SCANOUTHCLK           (SCANOUTHCLK)
+  );
+
+
+endmodule
